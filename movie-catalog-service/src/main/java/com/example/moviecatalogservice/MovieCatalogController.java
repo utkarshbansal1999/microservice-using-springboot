38f54c430038022673110c7a7f08007ad60e0940
utkarshbansal1999 <ub18.7.1999@gmail.com>
2020-04-05 01:34:11 +0530

    Only MovieInfoService microservice call

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -15,8 +17,18 @@ public class MovieCatalogController {
     @RequestMapping("/{userid}")
     public List<MovieCatalog> getCatalog(@PathVariable(value="userid") String userid)
     {
-        return Collections.singletonList(
-                new MovieCatalog("234","dostana",5)
+
+        List<RatingData> ratings= Arrays.asList(
+          new RatingData("123",5),
+          new RatingData("456",4)
         );
+
+        return ratings.stream().map(rating ->
+                (new MovieCatalog(rating.getMovieid(),"aliens",rating.getRating()))
+        ).collect(Collectors.toList());
+
+//        return Collections.singletonList(
+//                new MovieCatalog("234","dostana",5)
+//        );
     }
 }
