ff16852923a42c77b76e3730070402d653f2bcaa
utkarshbansal1999 <ub18.7.1999@gmail.com>
2020-04-06 00:54:33 +0530

    update using WebClient.Builder i.e. Reactive Web

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogController.java
@@ -1,10 +1,12 @@
 package com.example.moviecatalogservice;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -15,11 +17,17 @@ import java.util.stream.Collectors;
 @RequestMapping("/users")
 public class MovieCatalogController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userid}")
     public List<MovieCatalog> getCatalog(@PathVariable(value="userid") String userid)
     {
 
-        RestTemplate restTemplate=new RestTemplate();
+        //RestTemplate restTemplate=new RestTemplate();
 
         List<RatingData> ratings= Arrays.asList(
           new RatingData("123",5),
@@ -29,7 +37,16 @@ public class MovieCatalogController {
 
         return ratings.stream().map(rating ->
                 {
-                    MovieInfo movieInfo = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieid(), MovieInfo.class);
+                    //  MovieInfo movieInfo = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieid(), MovieInfo.class);
+                    //               OR
+
+                    MovieInfo movieInfo = webClientBuilder.build()
+                            .get()
+                            .uri("http://localhost:8082/movies/" + rating.getMovieid())
+                            .retrieve()
+                            .bodyToMono(MovieInfo.class)
+                            .block();
+
                     return new MovieCatalog(rating.getMovieid(),movieInfo.getName(),rating.getRating());
                 }
         ).collect(Collectors.toList());
